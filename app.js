307ec26b42b80f231cf5a6e361d806413854657c
musmanraza09 <musmanraza09@gmail.com>
2025-09-09 22:49:08 +0500

    JavaScript-Simple-Calculator

---
+++ app.js
@@ -0,0 +1,29 @@
+var display = document.getElementById("display");
+
+function append(value) {
+  var lastChar = display.value.slice(-1);
+  var operators = ["+", "-", "*", "/"];
+
+
+  if (operators.includes(value) && operators.includes(lastChar)) {
+    return;
+  }
+
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function delChar() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (e) {
+    display.value = "Error";
+  }
+}
